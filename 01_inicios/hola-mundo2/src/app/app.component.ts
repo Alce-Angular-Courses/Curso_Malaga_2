80e657f35107ba3806cb3f48e0bf0d164cc9e6e6
alce65 <alce65@hotmail.es>
2017-10-16 16:27:12 +0200

    Hola Mundos

---
+++ 01_inicios/hola-mundo2/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  curso = 'Angular 4.x';
+  formador = 'Alejandro Cerezo Lasne';
+  empresa = 'Icono Training Consulting';
+  fecha = '2017';
+}
